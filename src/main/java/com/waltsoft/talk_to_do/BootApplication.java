88b3cf061c7476cac737f2e6bcf66dfb30849491
walter azevedo <walter.azevedo@cyan-agro.com>
2026-02-21 20:32:02 -0300

    Fix dependencies and error inthe directories of changelogs

--- src/main/java/com/waltsoft/talk_to_do/BootApplication.java
+++ src/main/java/com/waltsoft/talk_to_do/BootApplication.java
@@ -7,38 +7,34 @@ import org.apache.commons.logging.LogFactory;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.persistence.autoconfigure.EntityScan;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.annotation.ComponentScan;
 import picocli.CommandLine;
 
 @SpringBootApplication
-@ComponentScan(basePackages = "com.waltsoft")
-@EntityScan(basePackages = "com.waltsoft")
-@CommandLine.Command(name = "run",
-        mixinStandardHelpOptions = true,
-        version = "1.0",
-        description = "Run commands")
+@ComponentScan(basePackages = "com.waltsoft.talk_to_do")
+@EntityScan(basePackages = "com.waltsoft.talk_to_do")
+@CommandLine.Command(name = "run", mixinStandardHelpOptions = true, version = "1.0", description = "Run commands")
 public class BootApplication implements CommandLineRunner {
 
-    private static final Log LOGGER = LogFactory.getLog(BootApplication.class);
+	private static final Log LOGGER = LogFactory.getLog(BootApplication.class);
 
-    public static void main(String[] args) {
-        SpringApplication application = new SpringApplication(BootApplication.class);
-        application.run(args);
-    }
+	public static void main(String[] args) {
+		SpringApplication application = new SpringApplication(BootApplication.class);
+		application.run(args);
+	}
 
-    @Override
-    public void run(String... args) {
-        try {
-            CommandLine commandLine = new CommandLine(new BootApplication());
-            commandLine.addSubcommand(new StartChatCommand());
-            commandLine.execute(args);
-        } catch (Exception exception) {
-            LOGGER.error(exception);
-            exception.printStackTrace();
-            System.exit(1);
-        }
-    }
+	@Override
+	public void run(String... args) {
+		try {
+			CommandLine commandLine = new CommandLine(new BootApplication());
+			commandLine.addSubcommand(new StartChatCommand());
+			commandLine.execute(args);
+		} catch (Exception exception) {
+			LOGGER.error(exception);
+			System.exit(1);
+		}
+	}
 
 }
 
--- src/main/java/com/waltsoft/talk_to_do/business/basic/BasicService.java
+++ src/main/java/com/waltsoft/talk_to_do/business/basic/BasicService.java
@@ -11,70 +11,70 @@ import java.util.Collection;
 import java.util.List;
 import java.util.Optional;
 
-public interface BasicService<T extends BasicEntity, ID> {
+public interface BasicService<T extends BasicEntity, I> {
 
-    default List<T> findAll() {
-        return getRepository().findAll();
-    }
+	default List<T> findAll() {
+		return getRepository().findAll();
+	}
 
-    JpaRepository<T, ID> getRepository();
+	JpaRepository<T, I> getRepository();
 
-    default List<T> findAll(Sort sort) {
-        return getRepository().findAll(sort);
-    }
+	default List<T> findAll(Sort sort) {
+		return getRepository().findAll(sort);
+	}
 
-    default Page<T> findAll(final Pageable pageable) {
-        return getRepository().findAll(pageable);
-    }
+	default Page<T> findAll(final Pageable pageable) {
+		return getRepository().findAll(pageable);
+	}
 
-    default Optional<T> findById(final ID id) {
-        return getRepository().findById(id);
-    }
+	default Optional<T> findById(final I id) {
+		return getRepository().findById(id);
+	}
 
-    default List<T> findByIds(final List<ID> ids) {
-        return getRepository().findAllById(ids);
-    }
+	default List<T> findByIds(final List<I> ids) {
+		return getRepository().findAllById(ids);
+	}
 
-    default boolean exists(final ID id) {
-        return getRepository().existsById(id);
-    }
+	default boolean exists(final I id) {
+		return getRepository().existsById(id);
+	}
 
-    default Long count() {
-        return getRepository().count();
-    }
+	default Long count() {
+		return getRepository().count();
+	}
 
-    @Transactional
-    default void delete(final T entity) {
-        getRepository().delete(entity);
-    }
+	@Transactional
+	default void delete(final T entity) {
+		getRepository().delete(entity);
+	}
 
-    @Transactional
-    default void delete(final Collection<? extends T> entities) {
-        getRepository().deleteAll(entities);
-    }
+	@Transactional
+	default void delete(final Collection<? extends T> entities) {
+		getRepository().deleteAll(entities);
+	}
 
-    @Transactional
-    default void deleteAll() {
-        getRepository().deleteAll();
-    }
+	@Transactional
+	default void deleteAll() {
+		getRepository().deleteAll();
+	}
 
-    @Transactional
-    default void deleteInBatch(final Collection<T> entities) {
-        getRepository().deleteAllInBatch(entities);
-    }
+	@Transactional
+	default void deleteInBatch(final Collection<T> entities) {
+		getRepository().deleteAllInBatch(entities);
+	}
 
-    @Transactional
-    default T save(final T entity) {
-        return getRepository().save(entity);
-    }
+	@Transactional
+	default T save(final T entity) {
+		return getRepository().save(entity);
+	}
 
-    @Transactional
-    default T saveAndFlush(final T entity) {
-        return getRepository().saveAndFlush(entity);
-    }
+	@Transactional
+	default T saveAndFlush(final T entity) {
+		return getRepository().saveAndFlush(entity);
+	}
 
-    @Transactional
-    default List<T> save(final Collection<T> entities) {
-        return getRepository().saveAll(entities);
-    }
+	@Transactional
+	default List<T> save(final Collection<T> entities) {
+		return getRepository().saveAll(entities);
+	}
 }
