515b7ca7ab01cbdcd7bfc91ec7fdb4f4bc7935f5
walter azevedo <walter.azevedo@cyan-agro.com>
2026-03-01 20:32:34 -0300

    Created list of tasks, list of categories and insert categories functions for A.I. and improved the context prompt

--- src/main/java/com/waltsoft/talk_to_do/business/task_priority/TaskPriorityService.java
+++ src/main/java/com/waltsoft/talk_to_do/business/task_priority/TaskPriorityService.java
@@ -4,8 +4,36 @@ package com.waltsoft.talk_to_do.business.task_priority;
 import com.waltsoft.talk_to_do.business.basic.BasicService;
 import com.waltsoft.talk_to_do.entity.task_priority.TaskPriority;
 import com.waltsoft.talk_to_do.entity.task_priority.enums.TaskPriorityCodeEnum;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Service;
 
-public interface TaskPriorityService extends BasicService<TaskPriority, Long> {
+import java.util.List;
 
-    TaskPriority findByCode(TaskPriorityCodeEnum code);
+@Service
+public class TaskPriorityService implements BasicService<TaskPriority, Long> {
+
+    private final TaskPriorityRepository repository;
+
+    @Autowired
+    public TaskPriorityService(final TaskPriorityRepository repository) {
+        this.repository = repository;
+    }
+
+    @Override
+    public JpaRepository<TaskPriority, Long> getRepository() {
+        return repository;
+    }
+
+    public TaskPriority findByCode(TaskPriorityCodeEnum code) {
+        List<TaskPriority> all = findAll();
+        return all
+                .stream()
+                .filter(p -> p
+                        .getCode()
+                        .equals(code))
+                .findFirst()
+                .get();
+    }
+
 }
--- src/main/java/com/waltsoft/talk_to_do/business/task_status/TaskStatusService.java
+++ src/main/java/com/waltsoft/talk_to_do/business/task_status/TaskStatusService.java
@@ -4,7 +4,36 @@ package com.waltsoft.talk_to_do.business.task_status;
 import com.waltsoft.talk_to_do.business.basic.BasicService;
 import com.waltsoft.talk_to_do.entity.task_status.TaskStatus;
 import com.waltsoft.talk_to_do.entity.task_status.enums.TaskStatusCodeEnum;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Service;
 
-public interface TaskStatusService extends BasicService<TaskStatus, Long> {
-    TaskStatus findByCode(TaskStatusCodeEnum code);
+import java.util.List;
+
+@Service
+public class TaskStatusService implements BasicService<TaskStatus, Long> {
+
+    private final TaskStatusRepository repository;
+
+    @Autowired
+    public TaskStatusService(final TaskStatusRepository repository) {
+        this.repository = repository;
+    }
+
+    @Override
+    public JpaRepository<TaskStatus, Long> getRepository() {
+        return repository;
+    }
+
+    public TaskStatus findByCode(TaskStatusCodeEnum code) {
+        List<TaskStatus> all = findAll();
+        return all
+                .stream()
+                .filter(p -> p
+                        .getCode()
+                        .equals(code))
+                .findFirst()
+                .get();
+    }
+
 }
