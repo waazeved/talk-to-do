f86aec5fcdc4f1db2a187e7fc23424a9229c48d2
waazeved <walter.azvdo@gmail.com>
2026-03-01 22:08:51 -0300

    Created update task function and improved readme

--- src/main/java/com/waltsoft/talk_to_do/business/ai_agent/AIAgentService.java
+++ src/main/java/com/waltsoft/talk_to_do/business/ai_agent/AIAgentService.java
@@ -2,14 +2,13 @@ package com.waltsoft.talk_to_do.business.ai_agent;
 
 import com.waltsoft.talk_to_do.business.task.TaskService;
 import com.waltsoft.talk_to_do.business.task_category.TaskCategoryService;
-import com.waltsoft.talk_to_do.business.task_priority.TaskPriorityService;
-import com.waltsoft.talk_to_do.business.task_status.TaskStatusService;
 import com.waltsoft.talk_to_do.dot_env.DotEnv;
 import org.springframework.ai.chat.client.ChatClient;
 import org.springframework.ai.chat.client.advisor.MessageChatMemoryAdvisor;
 import org.springframework.ai.chat.memory.ChatMemory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.core.io.Resource;
 import org.springframework.stereotype.Service;
 
@@ -32,15 +31,11 @@ public class AIAgentService {
                           ChatMemory chatMemory,
                           DotEnv dotEnv,
                           TaskService taskService,
-                          TaskStatusService taskStatusService,
-                          TaskCategoryService taskCategoryService,
-                          TaskPriorityService taskPriorityService) {
+                          TaskCategoryService taskCategoryService) {
         this.chatClient = makeChatClient(chatClientBuilder, chatMemory);
         this.username = dotEnv.getUsername(); //In the future, the user will authenticate to enter in the system and username will come from database
         this.taskService = taskService;
-        this.taskStatusService = taskStatusService;
         this.taskCategoryService = taskCategoryService;
-        this.taskPriorityService = taskPriorityService;
     }
 
 
@@ -53,6 +48,7 @@ public class AIAgentService {
                 .build();
     }
 
+    @Cacheable(value = "aiChatCache", key = "#userInput")
     public String chat(String userInput) {
 
         return chatClient
--- src/main/java/com/waltsoft/talk_to_do/business/task/TaskService.java
+++ src/main/java/com/waltsoft/talk_to_do/business/task/TaskService.java
@@ -1,23 +1,27 @@
 package com.waltsoft.talk_to_do.business.task;
 
 
+import com.waltsoft.talk_to_do.Exceptions.ai.InvalidEntityPropertyException;
 import com.waltsoft.talk_to_do.business.basic.BasicService;
 import com.waltsoft.talk_to_do.business.task_category.TaskCategoryService;
 import com.waltsoft.talk_to_do.business.task_priority.TaskPriorityService;
 import com.waltsoft.talk_to_do.business.task_status.TaskStatusService;
 import com.waltsoft.talk_to_do.dto.task.TaskDto;
 import com.waltsoft.talk_to_do.dto.task.TaskInsertDto;
+import com.waltsoft.talk_to_do.dto.task.TaskUpdateDto;
 import com.waltsoft.talk_to_do.entity.task.Task;
 import com.waltsoft.talk_to_do.entity.task_priority.TaskPriority;
 import com.waltsoft.talk_to_do.entity.task_status.TaskStatus;
 import org.springframework.ai.tool.annotation.Tool;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cache.CacheManager;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.time.LocalDateTime;
 import java.util.List;
+import java.util.Optional;
 import java.util.Set;
 import java.util.stream.Collectors;
 
@@ -28,13 +32,15 @@ public class TaskService implements BasicService<Task, Long> {
     private final TaskStatusService taskStatusService;
     private final TaskCategoryService taskCategoryService;
     private final TaskPriorityService taskPriorityService;
+    private final CacheManager cacheManager;
 
     @Autowired
-    public TaskService(final TaskRepository repository, TaskStatusService taskStatusService, TaskCategoryService taskCategoryService, TaskPriorityService taskPriorityService) {
+    public TaskService(final TaskRepository repository, TaskStatusService taskStatusService, TaskCategoryService taskCategoryService, TaskPriorityService taskPriorityService, CacheManager cacheManager) {
         this.repository = repository;
         this.taskStatusService = taskStatusService;
         this.taskCategoryService = taskCategoryService;
         this.taskPriorityService = taskPriorityService;
+        this.cacheManager = cacheManager;
     }
 
     @Override
@@ -62,6 +68,45 @@ public class TaskService implements BasicService<Task, Long> {
                         .get());
 
         repository.save(task);
+
+        cacheManager
+                .getCache("aiChatCache")
+                .clear();
+
+        return "New task created with success.";
+
+    }
+
+    @Transactional
+    @Tool(name = "updateTask", description = "Update a new task in the database")
+    public String update(TaskUpdateDto updateDto) {
+
+        Optional<Task> taskOptional = findById(updateDto.id());
+
+        if (taskOptional.isEmpty()) {
+            throw new InvalidEntityPropertyException("Invalid task id");
+        }
+
+        TaskPriority priority = this.taskPriorityService.findByCode(updateDto.taskPriorityCode());
+        TaskStatus status = this.taskStatusService.findByCode(updateDto.taskStatusCode());
+
+        Task task = taskOptional
+                .get()
+                .setTitle(updateDto.title())
+                .setDescription(updateDto.description())
+                .setPriority(priority)
+                .setStatus(status)
+                .setStartDateTime(LocalDateTime.parse(updateDto.startDateTime()))
+                .setEndDateTime(LocalDateTime.parse(updateDto.endDateTime()))
+                .setCategory(this.taskCategoryService
+                        .findById(updateDto.taskCategoryId())
+                        .get());
+
+        repository.save(task);
+
+        cacheManager
+                .getCache("aiChatCache")
+                .clear();
 
         return "New task created with success.";
 
